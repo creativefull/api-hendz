e4f2dc5bd1cc902fdc9fd5028de553624a07c141
Shodiqul Muzaki <user.creativefull@gmail.com>
2017-11-01 09:12:43 +0700

    prod

--- index.js
+++ index.js
@@ -5,15 +5,25 @@ const config = require('./config.json')
 const {createConnection} = require('mysql')
 const numCPUs = require('os').cpus().length
 const xmpp = require('simple-xmpp')
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
 // CONNECT DATABASE
 let conn = createConnection(config.database)
 conn.connect()
 global.conn = conn
 global.xmpp = xmpp
+global.io = io
 
 // CONNECT JABBER
 jabber.connect()
+
+// CONNECT SOCKET
+io.on('connection', (socket) => {
+	global.socket = socket
+	console.log("User connected")
+})
 
 const {startPool} = require('./lib/pool')
 
@@ -27,4 +37,4 @@ new CronJob('* * * * * *', () => {
 require('./lib/receiveChat')
 
 // RUN CLUSTER PROCESS
-console.log("Application running")
+http.listen(8089, () => console.log("Application running on port", 8089))
